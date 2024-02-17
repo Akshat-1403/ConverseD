0a21836296e7d434ac54af7f91fbef8447e9dedf
Akshat-1403 <akshat.tripathi14403@gmail.com>
2024-02-17 18:01:13 +0530

    Mongo setup done,controllers and routes init

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+    
+};
+
+export const login = (req,res) => {
+    res.send("Hello from login route")
+};
+
+export const logout = (req,res) => {
+    res.send("Hello from logout route")
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app=express();
+dotenv.config();
 
-app.listen(5000, ()=>{
-    console.log("Server up and running on port 5000");
-})
+const PORT = process.env.PORT ||5000;
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, ()=>{
+    connectToMongoDB();
+    console.log(`Server up and running on port ${PORT}`)
+});
