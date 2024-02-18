a2ec505d32ad842ea9f99807a16e1bc37634766c
Akshat-1403 <akshat.tripathi14403@gmail.com>
2024-02-18 17:43:21 +0530

    Complete signup func, JWT init

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT ||5000;
 
 const app = express();
+app.use(express.json());
+const PORT = process.env.PORT ||5000;
 
-app.get("/", (req,res)=>{
-    res.send("Hello World!");
-});
+dotenv.config();
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth", authRoutes)
+
+
+
+// app.get("/", (req,res)=>{
+//     res.send("Hello World!");
+// });
+
+
 
 app.listen(PORT, ()=>{
     connectToMongoDB();
