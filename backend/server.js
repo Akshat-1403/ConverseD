337caf9f36f0247ff3c756fcd3dbaa94a45164c0
Akshat-1403 <akshat.tripathi14403@gmail.com>
2024-02-26 23:51:39 +0530

    FE completed(state mgmt), Socket completed

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,29 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
 
-
-const app = express();
-const PORT = process.env.PORT ||5000;
-
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
-app.use(cookieParser());    
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("/", (req,res)=>{
-//     res.send("Hello World!");
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-
-
-app.listen(PORT, ()=>{
-    connectToMongoDB();
-    console.log(`Server up and running on port ${PORT}`)
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server up and running on port ${PORT}`);
 });
